2a8ea1e28f3832c6ac2d6abf849ad95bed549832
SvinoKot <aleksey.volegoff@yandex.ru>
2025-11-26 09:43:24 +0300

    Update Часы.py

--- Часы.py
+++ Часы.py
@@ -1,8 +1,9 @@
 ph=''
 pm=''
+vr=""
 s=input()
 c=[]
-if len(s)==5:
+if len(s)==5 or len(s)==4:
     for x in s.split():
         c.append(int(x))
     h=c[0]
@@ -16,16 +17,25 @@ if len(s)==5:
             print("полночь")
         else:
             if h%10==1:
-                ph=str(h)+"час"
+                ph=str((h-1)%12+1)+" час "
             elif h%10>1 and h%10<5:
-                ph=str(h)+"часа"
+                ph=str((h-1)%12+1)+" часа "
             else:
-                ph=str(h)+"часов"
+                ph=str((h-1)%12+1)+" часов "
             if m%10==0:
-                pm="ровно"
-            elif m%10==1:
-                pm=str(m)+"минута"
-            elif m%10>1 and m%10<5:
-                ph=str(m)+"минуты"
+                pm=" ровно "
+            elif m%10==1 and m//10!=1:
+                pm=str(m)+" минута "
+            elif m%10>1 and m%10<5 and m//10!=1:
+                pm=str(m)+" минуты "
             else:
-                ph=str(m)+"минуты"
+                pm=str(m)+" минут "
+            if h//6==0:
+                vr="ночи "
+            elif h//6==1:
+                vr="утра "
+            elif h//6==2:
+                vr="дня "
+            else:
+                vr="вечера"
+        print(ph+pm+vr)
